0bb3c244aca2a1c4a8467d22b82fed9a7dc3f823
volutedberet424 <60577740+volutedberet424@users.noreply.github.com>
2020-10-13 00:00:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.on('ready', () =>{
     console.log('# Bot by:Volutedberet #')
     console.log('#                     #')
     console.log('#######################')
-    client.user.setActivity("Prefix: m!") 
+    client.user.setActivity("Prefix: m!");
 })
 
 bot.on('message', msg=>{
